bb5d0d8091702d9d2d54ab3b099dafeec54b9b5c
Claudio Poli <masterkain@gmail.com>
2011-07-30 19:54:56 +0200

    Add files

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'mongo'
+gem 'bson_ext'
+gem 'mysql2'
---
+++ mysql2mongo.rb
@@ -0,0 +1,39 @@
+require "rubygems"
+require "bundler/setup"
+require "mysql2"
+require "mongo"
+
+MYSQL_SETTINGS = {
+  host:     "127.0.0.1",
+  username: "root",
+  password: "",
+  database: "myapp_production",
+  encoding: "utf8"
+}
+
+MONGO_SETTINGS = {
+  schema:   "myapp_production"
+}
+
+mysql_client = Mysql2::Client.new(MYSQL_SETTINGS)
+mysql_client.query_options.merge!(symbolize_keys: true)
+
+mongo_client = Mongo::Connection.new.db(MONGO_SETTINGS[:schema])
+
+if MONGO_SETTINGS.has_key?(:username) && MONGO_SETTINGS.has_key?(:password)
+  raise SystemExit, "Cannot connect to mongo, provided keys are not valid" unless mongo_client.authenticate(MONGO_SETTINGS[:username], MONGO_SETTINGS[:password])
+end
+
+puts "Migrating from: #{MYSQL_SETTINGS[:database]}"
+
+mysql_query_string = "select * from information_schema.tables where table_schema='#{MYSQL_SETTINGS[:database]}'"
+mysql_client.query(mysql_query_string).each do |row|
+  current_table = row[:TABLE_NAME]
+  puts "Working on: #{row[:TABLE_NAME]}"
+  inner_qs = "select * from #{current_table}"
+
+  current_collection = mongo_client.collection(current_table)
+  mysql_client.query(inner_qs).each do |result_row|
+    current_collection.insert(result_row)
+  end
+end
